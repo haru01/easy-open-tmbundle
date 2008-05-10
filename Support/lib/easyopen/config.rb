1b2266b859cd438622bc5dfd62a1e888027a9958
IenagaEiji <eiji.ienaga@gmail.com>
2008-05-11 06:45:22 +0900

    configを導入した

---
+++ Support/lib/easyopen/config.rb
@@ -0,0 +1,25 @@
+module EasyOpen
+  class Config
+    class << self
+      def defaults
+        save_dir = "#{ENV["HOME"]}/.easyopen_tmbundle#{ENV["TM_PROJECT_DIRECTORY"]}"
+        @defaults ||= {
+          :project_dir      =>  ENV["TM_PROJECT_DIRECTORY"],
+          :current_word     =>  ENV['TM_CURRENT_WORD'],
+          :save_dir         => save_dir,
+          :def_location_dump => "#{save_dir}/def_location.dump",
+          :call_stack_dump    => "#{save_dir}/call_stack.dump",
+          :current_location => {:file => ENV["TM_FILEPATH"], :line => ENV["TM_LINE_NUMBER"], :column => ENV["TM_COLUMN_NUMBER"]},
+        }
+      end
+
+      def setup(settings = {})
+        @configuration = defaults.merge(settings)
+      end
+
+      def [](key)
+        (@configuration ||= defaults)[key]
+      end
+    end
+  end
+end
---
+++ Support/spec/easyopen/config_spec.rb
@@ -0,0 +1,24 @@
+require File.dirname(__FILE__) + "/../../lib/easyopen/config"
+
+module EasyOpen
+  describe Config do
+    it "should default" do
+      Config.defaults[:project_dir].should == ENV["TM_PROJECT_DIRECTORY"]
+      Config.defaults[:current_word].should == ENV['TM_CURRENT_WORD']
+      Config.defaults[:save_dir].should == "#{ENV["HOME"]}/.easyopen_tmbundle#{ENV["TM_PROJECT_DIRECTORY"]}"
+      Config.defaults[:def_location_dump].should == "#{ENV["HOME"]}/.easyopen_tmbundle#{ENV["TM_PROJECT_DIRECTORY"]}/def_location.dump"
+      Config.defaults[:call_stack_dump].should == "#{ENV["HOME"]}/.easyopen_tmbundle#{ENV["TM_PROJECT_DIRECTORY"]}/call_stack.dump"
+      Config.defaults[:current_location].should == {:file => ENV["TM_FILEPATH"], :line => ENV["TM_LINE_NUMBER"], :column => ENV["TM_COLUMN_NUMBER"]}
+    end
+    
+    it "should return config value" do
+      Config[:project_dir].should == ENV["TM_PROJECT_DIRECTORY"]
+    end
+    
+    it "should setup config value" do
+      Config.setup({:project_dir => "new_dir", :hoge => "hoge"})
+      Config[:project_dir].should == "new_dir"
+      Config[:hoge].should == "hoge"
+    end
+  end
+end
