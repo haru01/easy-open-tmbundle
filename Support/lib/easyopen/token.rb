2637fe540f997c4da303c3e5f0b84ef8f83ad6c7
IenagaEiji <eiji.ienaga@gmail.com>
2008-09-09 07:17:26 +0900

    append module 'EaseOpen'

--- Support/lib/easyopen/token.rb
+++ Support/lib/easyopen/token.rb
@@ -1,40 +1,42 @@
-class JavaScriptToken
-  def tokenize(line)
-    if m = /^([^\s]*)\s*=\s*function\s*(\(.*\)).*\{.*$/.match(line)
-      name = m[1].split(".").last
-      tmp = m[1].split(".")
-      tmp.pop
-      pre = tmp.join(".")
-      pre += "."
-      {
-        :names => m[1].split(".").last,
-        :pre_first_str => pre
-      }
-    elsif m = /^([\s]*)(.*):\s*function.*$/.match(line)
-      {
-        :names => m[2],
-        :pre_first_str => m[1]
-      }
+module EasyOpen
+  class JavaScriptToken
+    def tokenize(line)
+      if m = /^([^\s]*)\s*=\s*function\s*(\(.*\)).*\{.*$/.match(line)
+        name = m[1].split(".").last
+        tmp = m[1].split(".")
+        tmp.pop
+        pre = tmp.join(".")
+        pre += "."
+        {
+          :names => m[1].split(".").last,
+          :pre_first_str => pre
+        }
+      elsif m = /^([\s]*)(.*):\s*function.*$/.match(line)
+        {
+          :names => m[2],
+          :pre_first_str => m[1]
+        }
+      end
     end
   end
-end
 
-class RubyToken
-  def tokenize(line)
-    if m = /(^\s*(class|def|module)\s*)([\w:\.]*)(.*)$/.match(line)
-      names = if m[3].include?("self.")
-        m[3].gsub("self.", "")
-      else
-        m[3].split("::")
+  class RubyToken
+    def tokenize(line)
+      if m = /(^\s*(class|def|module)\s*)([\w:\.]*)(.*)$/.match(line)
+        names = if m[3].include?("self.")
+          m[3].gsub("self.", "")
+        else
+          m[3].split("::")
+        end
+      
+        pre_first_str = m[1]
+        pre_first_str += "self." if m[3].include?("self.")
+      
+        {
+          :pre_first_str => pre_first_str,
+          :names => names,
+        }
       end
-      
-      pre_first_str = m[1]
-      pre_first_str += "self." if m[3].include?("self.")
-      
-      {
-        :pre_first_str => pre_first_str,
-        :names => names,
-      }
     end
   end
 end
--- Support/spec/easyopen/token_spec.rb
+++ Support/spec/easyopen/token_spec.rb
@@ -1,59 +1,61 @@
 require File.dirname(__FILE__) + "/../../lib/easyopen/token"
 
-describe JavaScriptToken do
-  before(:each) do
-    @token = JavaScriptToken.new
+module EasyOpen
+  describe JavaScriptToken do
+    before(:each) do
+      @token = JavaScriptToken.new
+    end
+  
+    it "should tokeninze 'JSSpec.Executor = function(target, onSuccess, onException) {'" do
+      result = @token.tokenize('JSSpec.Executor = function(target, onSuccess, onException) {')
+      result[:names].should == "Executor"
+      result[:pre_first_str].should == "JSSpec."      
+    end
+  
+    it "should tokeninze 'JSSpec.Executor = function (target, onSuccess, onException) {'" do
+      result = @token.tokenize('JSSpec.Executor = function (target, onSuccess, onException) {')
+      result[:names].should == "Executor"
+      result[:pre_first_str].should == "JSSpec."      
+    end
+  
+    it "should tokenize '  grep: function(filter, iterator, context) {'" do
+      result = @token.tokenize('  grep: function(filter, iterator, context) {')
+      result[:names].should == "grep"
+      result[:pre_first_str].should == "  "          
+    end
   end
   
-  it "should tokeninze 'JSSpec.Executor = function(target, onSuccess, onException) {'" do
-    result = @token.tokenize('JSSpec.Executor = function(target, onSuccess, onException) {')
-    result[:names].should == "Executor"
-    result[:pre_first_str].should == "JSSpec."      
-  end
+  describe RubyToken do
+    before(:each) do
+      @token = RubyToken.new
+    end
   
-  it "should tokeninze 'JSSpec.Executor = function (target, onSuccess, onException) {'" do
-    result = @token.tokenize('JSSpec.Executor = function (target, onSuccess, onException) {')
-    result[:names].should == "Executor"
-    result[:pre_first_str].should == "JSSpec."      
-  end
+    it "should tokenize '	def self.hogefuga(hoge, foo)'" do
+      result = @token.tokenize('	def self.hogefuga(hoge, foo)')
+      result[:names].should == "hogefuga"
+      result[:pre_first_str].should == "	def self."
+    end
   
-  it "should tokenize '  grep: function(filter, iterator, context) {'" do
-    result = @token.tokenize('  grep: function(filter, iterator, context) {')
-    result[:names].should == "grep"
-    result[:pre_first_str].should == "  "          
-  end
-end
+    it "should tokenize '	def open(hoge, foo)'" do
+      line = "	def open(hoge, foo)"
+      @token.tokenize(line)[:pre_first_str].should == "	def "
+      @token.tokenize(line)[:names].should == ["open"]
+    end
   
-describe RubyToken do
-  before(:each) do
-    @token = RubyToken.new
-  end
+    it "should tokenize 'module Hoge::Hogeogeoge'" do
+      line = 'module Hoge::Hogeogeoge'
+      @token.tokenize(line)[:pre_first_str].should == "module "
+      @token.tokenize(line)[:names].should == ["Hoge", "Hogeogeoge"]
+    end
   
-  it "should tokenize '	def self.hogefuga(hoge, foo)'" do
-    result = @token.tokenize('	def self.hogefuga(hoge, foo)')
-    result[:names].should == "hogefuga"
-    result[:pre_first_str].should == "	def self."
-  end
+    it "should tokenize return nil if not include def module class" do
+      line = '      '
+      @token.tokenize(line).should be_nil
+    end
   
-  it "should tokenize '	def open(hoge, foo)'" do
-    line = "	def open(hoge, foo)"
-    @token.tokenize(line)[:pre_first_str].should == "	def "
-    @token.tokenize(line)[:names].should == ["open"]
-  end
-  
-  it "should tokenize 'module Hoge::Hogeogeoge'" do
-    line = 'module Hoge::Hogeogeoge'
-    @token.tokenize(line)[:pre_first_str].should == "module "
-    @token.tokenize(line)[:names].should == ["Hoge", "Hogeogeoge"]
-  end
-  
-  it "should tokenize return nil if not include def module class" do
-    line = '      '
-    @token.tokenize(line).should be_nil
-  end
-  
-  it "should tokenize return nil if comment def module class" do
-    line = ' # def hoge'
-    @token.tokenize(line).should be_nil
+    it "should tokenize return nil if comment def module class" do
+      line = ' # def hoge'
+      @token.tokenize(line).should be_nil
+    end
   end
 end
