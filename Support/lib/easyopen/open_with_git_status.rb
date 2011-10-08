4d56b4175f7dc2859275cf579d853573285374a0
eiji.ienaga <eiji.ienaga@gmail.com>
2011-10-09 08:10:40 +0900

    refactor

--- Support/lib/easyopen/open_with_git_status.rb
+++ Support/lib/easyopen/open_with_git_status.rb
@@ -5,32 +5,21 @@ require File.dirname(__FILE__) + '/repository'
 module EasyOpen
   class OpenWithGitStatus
     include EasyOpen::UI
-    
+
     def open
-      git_status_str = %x[pushd "#{ENV['TM_PROJECT_DIRECTORY']}";  git status]
+      git_status_str = %x[pushd "#{ENV['TM_PROJECT_DIRECTORY']}";  git status -s]
       menu_infos = menu_infos(git_status_str)
       open_menu_list(menu_infos)
     end
-    
+
     def menu_infos(git_status_str)
-      git_status_str.split("\n").inject([]) do |files, line|                
-        if m = /^#\trenamed:.+\s->\s(.+)$/.match(line)
-          files << { :file => "#{ENV['TM_PROJECT_DIRECTORY']}/#{m[1]}", 
-                     :display => "renamed: #{m[1]}" }
-        elsif m = /^#\tmodified:\s+(.+)$/.match(line)
-          files << { :file => "#{ENV['TM_PROJECT_DIRECTORY']}/#{m[1]}", 
-                     :display => "modified: #{m[1]}" }
-        elsif m = /^#\tnew file:\s+(.+)$/.match(line)
-          files << { :file => "#{ENV['TM_PROJECT_DIRECTORY']}/#{m[1]}", 
-                    :display => "new file: #{m[1]}" }
-        elsif m = /^#\tdeleted:\s+(.+)$/.match(line)
-          # ignore
-        elsif m = /^#\t([^()]+)$/.match(line)
-          files << { :file  => "#{ENV['TM_PROJECT_DIRECTORY']}/#{m[1]}", 
-                     :display => "untrancked: #{m[1]}" }
-        end
-        files
-      end      
+      git_status_str.split("\n")
+                    .select{|line| line.start_with?(" M") or line.start_with?("A") or line.start_with?("??") or line.start_with?(" D") }
+                    .inject([]) do |files, line|
+        status, filepath = line.split(" ").map(&:strip)
+        files << { :file => "#{ENV['TM_PROJECT_DIRECTORY']}/#{filepath}",
+                 :display => "#{line}" }
+      end
     end
   end
 end
--- Support/spec/easyopen/open_with_git_status_spec.rb
+++ Support/spec/easyopen/open_with_git_status_spec.rb
@@ -11,34 +11,15 @@ module EasyOpen
   describe "subject" do
     before(:each) do
       puts File.dirname(__FILE__) + "/../../lib/easyopen/open_with_git_status"
-      
+
       @subject = OpenWithGitStatus.new
-      
-      @git_status_str =<<GIT
-# On branch try-git-status
-# Changes to be committed:
-#   (use "git reset HEAD <file>..." to unstage)
-#
-#	new file:   Commands/open_with_git_status.tmCommand
-#	modified:   README
-#	deleted:    Support/spec/easyopen/open_with_git_status.rb
-#
-# Changes to be committed:
-#   (use "git reset HEAD <file>..." to unstage)
-#
-#	renamed:    Support/fixtures/ruby_code.rb -> Support/fixtures/renamed_ruby_code.rb
-#
-# Changed but not updated:
-#   (use "git add <file>..." to update what will be committed)
-#   (use "git checkout -- <file>..." to discard changes in working directory)
-#
-#	modified:   README
-#
-# Untracked files:
-#   (use "git add <file>..." to include in what will be committed)
-#
-#	hoge.txt
-GIT
+
+      @git_status_str =<<GIT_STATUS
+ D Support/lib/easyopen/open_mdfind.rb
+ M Support/lib/easyopen/open_with_git_status.rb
+?? untitled.rb
+A  untitled.rb
+GIT_STATUS
     end
     it "---- check manual" do
       TestUI.new.open_menu( @subject.menu_infos(@git_status_str) )
